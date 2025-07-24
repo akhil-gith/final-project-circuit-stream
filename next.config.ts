8d75bbb006ec92e279add9ba265658ef8773b0b2
akhil-gith <akhilsnalluri@gmail.com>
2025-07-24 11:58:48 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      },
-    ],
-  },
-};
-
 export default {
   output: "export",
   images: { unoptimized: true },
-  basePath: "final-project-circuit-stream",
-}
+  basePath: "/final-project-circuit-stream",
+};
