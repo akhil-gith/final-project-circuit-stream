725a27471bcafc95df6c47f8c518853f69e3c5d4
akhil-gith <akhilsnalluri@gmail.com>
2025-07-24 19:05:25 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,3 @@
 "use client";
 
 
-import { useState } from "react";
-
-type Animal = {
-  name: string;
-  diet?: string;
-  species?: string;
-  image?: string;
-  locations?: { lat: number; lon: number }[];
-  sightings?: { lat: number; lon: number; lastSeen: string }[];
-};
-
-const animalData: Animal[] = [
-  {
-    name: "Bear",
-    diet: "Omnivore",
-    species: "Mammal",
-    image: "/images/bear.jpg",
-    locations: [
-      { lat: 61.5240, lon: 105.3188 }, // Russia
-      { lat: 64.2008, lon: -149.4937 }, // Alaska
-    ],
-  }
-];
